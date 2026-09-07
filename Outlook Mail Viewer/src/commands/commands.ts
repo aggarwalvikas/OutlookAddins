d5a25738e96fe62d08e333069909060ad43fedf8
Vikas Aggarwal <vaggarwal@thomaspublishing.com>
2026-09-07 16:28:29 +0530

    Fix Office.js command typings

--- Outlook Mail Viewer/src/commands/commands.ts
+++ Outlook Mail Viewer/src/commands/commands.ts
@@ -1,3 +1,5 @@
+/// <reference types="office-js" />
+
 /*
  * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
  * See LICENSE in the project root for license information.
